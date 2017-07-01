16ef0a46ee6d1ed87b4b825f0ab70fc31db5a4d4
Kumar <528690@cognizant.com>
2017-07-01 14:44:24 +0530

    Signed-off-by: Kumar <528690@cognizant.com>

--- app/bookPage.js
+++ app/bookPage.js
@@ -1,42 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as bookActions from './actions/bookActions';
 
 class Book extends React.Component{
   constructor(props){
-    // Pass props back to parent
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(null, e);
   }
 
-  // Submit book handler
   submitBook(input){
-    alert('Submitted')
-  }
-
-  handleSubmit(e) {
-  	// Prevent request
-    e.preventDefault();
-    // Assemble inputs
-    var input = {title: titleInput.value};
-    // Call handler
-    this.submitBook(input);
-    // Reset form
-    e.target.reset();
+    this.props.createBook(input);
   }
 
   render(){
-    // Title input tracker
     let titleInput;
-    // return JSX
     return(
       <div>
         <h3>Books</h3>
         <ul>
-          {/* Traverse books array  */}
           {this.props.books.map((b, i) => <li key={i}>{b.title}</li> )}
         </ul>
         <div>
           <h3>Books Form</h3>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={e => {
+            e.preventDefault();
+            var input = {title: titleInput.value};
+            this.submitBook(input);
+            e.target.reset();
+          }}>
             <input type="text" name="title" ref={node => titleInput = node}/>
             <input type="submit" />
           </form>
@@ -46,4 +36,21 @@ class Book extends React.Component{
   }
 }
 
-export default Book;
+// Maps state from store to props
+const mapStateToProps = (state, ownProps) => {
+  return {
+    // You can now say this.props.books
+    books: state.books
+  }
+};
+
+// Maps actions to props
+const mapDispatchToProps = (dispatch) => {
+  return {
+  // You can now say this.props.createBook
+    createBook: book => dispatch(bookActions.createBook(book))
+  }
+};
+
+// Use connect to put them together
+export default connect(mapStateToProps, mapDispatchToProps)(Book);
